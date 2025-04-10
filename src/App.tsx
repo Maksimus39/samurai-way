aa61c6637471b1a414f04a165c481a0dc89edfb7
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-10 14:51:02 -0700

    lesson - 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./header/Header";
-import {Navbar} from "./navbar/Navbar";
-import {Profile} from "./profile/Profile";
+import {Header} from "./components/header/Header";
+import {Navbar} from "./components/navbar/Navbar";
+import {Profile} from "./components/profile/Profile";
 
 export const App = () => {
     return (
