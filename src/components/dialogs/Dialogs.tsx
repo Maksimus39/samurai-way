0c0698d3ea3a7d8465512dded40a8921ace5db46
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-14 15:27:49 -0700

    lesson - 21

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -4,8 +4,28 @@ import styles from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={styles.dialog}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>Maksim</div>
+                <div className={styles.dialog}>Larisa</div>
+                <div className={styles.dialog}>Andrey</div>
+                <div className={styles.dialog}>Bogdan</div>
+            </div>
+
+            <div className={styles.messageItems}>
+                <div className={styles.messages}>
+                    "Сынок, всегда помни: упорство и честность приведут к успеху."
+                </div>
+                <div className={styles.messages}>
+                    "Дорогая, верь в себя и никогда не бойся мечтать."
+                </div>
+                <div className={styles.messages}>
+                    "Мама, папа, спасибо за поддержку и веру в меня."
+                </div>
+                <div className={styles.messages}>
+                    "Брат, помоги мне, пожалуйста, с домашним заданием по математике."
+                </div>
+            </div>
         </div>
     );
 };
