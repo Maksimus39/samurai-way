86cea09efd529cefab12aaa6c08d47fa2b584c56
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-14 08:57:45 -0700

    lesson - 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,31 @@ import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {Messages} from "./components/navbar/messages/Messages";
+import {News} from "./components/navbar/news/News";
+import {Music} from "./components/navbar/music/Music";
+import {Settings} from "./components/navbar/settings/Settings";
 
 export const App = () => {
     return (
-        <div className="App-Wrapper">
-            <Header/>
-            <Navbar/>
+        <BrowserRouter>
+            <div className="App-Wrapper">
+                <Header/>
+                <Navbar/>
 
 
-            <div className={"App-Wrapper-content"}>
-                <Dialogs/>
-                {/*<Profile/>*/}
+                <div className={"App-Wrapper-content"}>
+                    <Route exact path="/profile" component={Profile}/>
+                    <Route exact path="/dialogs" component={Dialogs}/>
+                    <Route exact path="/messages" component={Messages}/>
+                    <Route exact path="/news" component={News}/>
+                    <Route exact path="/music" component={Music}/>
+                    <Route exact path="/settings" component={Settings}/>
+                </div>
+
             </div>
+        </BrowserRouter>
 
-        </div>
     );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import styles from "./Navbar.module.css"
+import {Dialogs} from "../dialogs/Dialogs";
 
 export const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <div className={styles.item}>
-                <a href="">Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={styles.item}>
-                <a href="">Messages</a>
+                <a href="/dialogs">Dialogs</a>
             </div>
             <div className={styles.item}>
-                <a href="">News</a>
+                <a href="/messages">Messages</a>
             </div>
             <div className={styles.item}>
-                <a href="">Music</a>
+                <a href="/news">News</a>
             </div>
             <div className={styles.item}>
-                <a href="">Settings</a>
+                <a href="/music">Music</a>
+            </div>
+            <div className={styles.item}>
+                <a href="/settings">Settings</a>
             </div>
         </div>
     );
 }
-
