2c7e8d750f52e2f98d71514c92deee1893420eb2
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-14 08:24:42 -0700

    lesson - 18

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
+import {Dialogs} from "./components/dialogs/Dialogs";
 
 export const App = () => {
     return (
         <div className="App-Wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+
+
+            <div className={"App-Wrapper-content"}>
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
+
         </div>
     );
 }
---
+++ src/components/dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Dialogs.module.css"
+
+
+export const Dialogs = () => {
+    return (
+        <div>
+            dialogs
+        </div>
+    );
+};
+
