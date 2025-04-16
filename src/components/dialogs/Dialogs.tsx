ac82e9db581e7e4e70ba08708c362947e2a1b528
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-16 00:49:33 -0700

    lesson - 24

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -4,29 +4,52 @@ import {NavLink} from "react-router-dom";
 import {DialogItem} from "./dialogItem/DialogItem";
 import {Message} from "./message/Message";
 
+
+type DialogItemArrayType = {
+    id: number
+    name: string
+}
+
+type MessageArrayType = {
+    id: number
+    message: string
+}
+
 export const Dialogs = () => {
+
+    const dialogItemArray: DialogItemArrayType[] = [
+        {id: 1, name: "Maksim"},
+        {id: 2, name: "Larisa"},
+        {id: 3, name: "Andrey"},
+        {id: 4, name: "Bogdan"}
+    ]
+
+    const messageArray: MessageArrayType[] = [
+        {id: 1, message: "Сынок, всегда помни: упорство и честность приведут к успеху."},
+        {id: 2, message: "Дорогая, верь в себя и никогда не бойся мечтать."},
+        {id: 3, message: "Мама, папа, спасибо за поддержку и веру в меня."},
+        {id: 4, message: "Брат, помоги мне, пожалуйста, с домашним заданием по математике."}
+    ]
+
     return (
         <div className={styles.dialog}>
             <div className={styles.dialogsItems}>
 
                 <div className={styles.dialog}>
-                  <DialogItem userId={1} name={"Maksim"}/>
-                  <DialogItem userId={2} name={"Larisa"}/>
-                  <DialogItem userId={3} name={"Andrey"}/>
-                  <DialogItem userId={4} name={"Bogdan"}/>
-                </div>
 
+                    {dialogItemArray.map((el) => {
+                        return <div key={el.id}><DialogItem userId={el.id} name={el.name}/></div>
+                    })}
+                </div>
             </div>
 
             <div className={styles.messageItems}>
-
                 <div className={styles.messages}>
-                    <Message message={"Сынок, всегда помни: упорство и честность приведут к успеху."}/>
-                    <Message message={"Дорогая, верь в себя и никогда не бойся мечтать."}/>
-                    <Message message={"Мама, папа, спасибо за поддержку и веру в меня."}/>
-                    <Message message={"Брат, помоги мне, пожалуйста, с домашним заданием по математике."}/>
-                </div>
 
+                    {messageArray.map((el) => {
+                        return <div key={el.id}><Message message={el.message}/></div>
+                    })}
+                </div>
             </div>
         </div>
     );
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import styles from './MyPosts.module.css';
 import {Post} from "./post/Post";
 
+type PostType = {
+    id: number
+    post: string
+    likes: number
+}
 export const MyPosts = () => {
+
+    const postArray: PostType[] = [
+        {id: 1, post: "Post-1", likes: 20},
+        {id: 2, post: "Post-2", likes: 40},
+    ]
+
+
     return (
         <div>
             My post
@@ -10,10 +22,10 @@ export const MyPosts = () => {
                 <input type="text" placeholder={"post"}/>
                 <button>Add post</button>
             </div>
-            <Post post={"Post-1"}
-                  likes={20}/>
-            <Post post={"Post-2"}
-                  likes={40}/>
+
+            {postArray.map(el => {
+                return <div key={el.id}><Post post={el.post} likes={el.likes}/></div>
+            })}
         </div>
     );
 }
